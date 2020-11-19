059ce023b8be99836b867a6f741419343d964ce2
Cai Leao <cainaleao.uk@gmail.com>
2020-11-19 20:08:01 +0000

    feat: remove default code from `<App />` and implement `<Challenge />`

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Box } from './components/Box';
+import { Challenge } from './features/Challenge';
+
+const ComponentWrapper = styled(Box)`
+    width: 512px;
+    height: 512px;
+`;
+
+export const App = () => {
+    return (
+        <ComponentWrapper>
+            <Challenge />
+        </ComponentWrapper>
+    );
 }
 
-export default App;
