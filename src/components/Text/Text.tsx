00edd85a948dd253b67314d7be6f06da35ac76a7
Cai Leao <cainaleao.uk@gmail.com>
2020-11-19 20:02:21 +0000

    feat: create `<Text />` component

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { Colors } from '../../themes';
+
+//TODO: add font-family here when we find out what it's suppose to be
+export const Text = styled.p`
+    font-size: 14px;
+    margin: 0;
+    padding: 0;
+    color: ${Colors.GREY_DARK};
+`;
