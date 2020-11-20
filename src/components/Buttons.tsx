7e84e2192a1ef998956c1c52d4f36ff578ed4490
Cai Leao <cainaleao.uk@gmail.com>
2020-11-20 02:20:01 +0000

    fix: change `<Button />` to use semantic  element  & improve styles

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -5,12 +5,19 @@ import { Colors } from '../themes';
 import { Box } from './Box';
 import { Text } from './Text';
 
-const Container = styled(Box)`
+const Container = styled.button`
+    display: flex;
+    flex: 1;
     background-color: ${Colors.YELLOW};
     align-items: center;
     justify-content: center;
     width: 144px;
     max-height: 48px;
+    cursor: pointer;
+    border: none;
+    &:focus {
+        outline: none;
+    }
 `;
 
 const Label = styled(Text)`
