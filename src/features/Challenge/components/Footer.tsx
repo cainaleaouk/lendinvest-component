fb08ae013277ab6a4a96385ae401156e673db424
Cai Leao <cainaleao.uk@gmail.com>
2020-11-20 02:09:54 +0000

    feat: connect `<Footer />` component

--- src/features/Challenge/components/Footer.tsx
+++ src/features/Challenge/components/Footer.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
+import { connect } from 'react-redux';
 
 import { Box } from '../../../components/Box';
 import { Text } from '../../../components/Text';
+import { RootState } from '../../../store';
+import { getTotalStr } from '../store/selectors';
 
 const Container = styled(Box)`
     justify-content: center;
@@ -17,10 +20,16 @@ interface StateProps {
     total: string;
 }
 
-export const Footer = ({total}: StateProps) => {
+const Footer = ({total}: StateProps) => {
     return (
         <Container>
             <Label>Total amount available for investments: <b>{total}</b></Label>
         </Container>
     )
 }
+
+const mapStateToProps = (state: RootState): StateProps => ({
+    total: getTotalStr(state),
+})
+
+export default connect(mapStateToProps)(Footer);
